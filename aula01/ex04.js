49d2403a41f0dcea199fd479b8d26031b5e6dbcd
LuizaoAntonio <luizantoniocaetanodasilva800@gmail.com>
2026-05-26 08:51:27 -0300

    ex04 fazendo

---
+++ aula01/ex04.js
@@ -0,0 +1,13 @@
+const input = require ('readline-sync')
+
+console.log("Digite um número para eu mostrar a tabuada de 1 a 10")
+
+let num1 = Number(input.question())
+
+for(let i = Number(0); i<= 10; i++){
+     let numa = Number(0)
+     let mult = Number(numa * num1)
+
+        console.log(``)
+
+}
