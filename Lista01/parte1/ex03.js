4ddfefb58dec9b7d636e281c770b2f2d3d803c30
YunoYuzuki <yunos90a@gmail.com>
2026-06-12 12:14:57 -0300

    não dale

---
+++ Lista01/parte1/ex03.js
@@ -0,0 +1,17 @@
+const input = require("readline-sync");
+
+let sn = Number(input.question("Bora de teste dnv??(1 para sim e 0 para não)")) 
+
+    if(sn === 1){
+        let num = Number(input.question("Boa meu amostradinho. Digita um número enfadonho aí pra gente ver se é ímpar\n"))
+    }
+    else if(sn === 0){
+        console.log("olk mano :( Vc me odeia?? :,(")
+    }
+    else{
+        console.log("Você realmente tem PCD, não é possível")
+    }
+
+    let SoN = (num % 2 === 0) ? "par dms" : ímpar;
+
+    console.log(`Seu número é ${SoN}`)
