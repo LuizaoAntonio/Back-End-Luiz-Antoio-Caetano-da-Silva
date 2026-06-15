bfe6cc9b79263bac0772a8764e19115ecf8ccd55
YunoYuzuki <yunos90a@gmail.com>
2026-06-15 11:07:00 -0300

    dalelistapt1-3

--- Lista01/parte1/ex03.js
+++ Lista01/parte1/ex03.js
@@ -4,6 +4,9 @@ let sn = Number(input.question("Bora de teste dnv??(1 para sim e 0 para não)"))
 
     if(sn === 1){
         let num = Number(input.question("Boa meu amostradinho. Digita um número enfadonho aí pra gente ver se é ímpar\n"))
+        let SoN = (num % 2 === 0) ? "par dms" : "ímpar";
+
+            console.log(`Seu número é ${SoN}`)
     }
     else if(sn === 0){
         console.log("olk mano :( Vc me odeia?? :,(")
@@ -12,6 +15,4 @@ let sn = Number(input.question("Bora de teste dnv??(1 para sim e 0 para não)"))
         console.log("Você realmente tem PCD, não é possível")
     }
 
-    let SoN = (num % 2 === 0) ? "par dms" : ímpar;
-
-    console.log(`Seu número é ${SoN}`)
+    
