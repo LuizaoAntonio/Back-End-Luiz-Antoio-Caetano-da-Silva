88e3c33c8415b2196fea61b8913867057f683f6f
LuizaoAntonio <luiz.caetano.silva16@escola.pr.gov.br>
2026-06-30 07:52:23 -0300

    dale03pt3

---
+++ aula03/ex03.js
@@ -0,0 +1,21 @@
+const input = require("readline-sync");
+
+const podutos = [
+    { id: 1, nome: "Cinquenta tons de taxa", preco: 50},
+    { id: 2, nome: "Largados e Taxados", preco: 2.79},
+    { id: 3, nome: "Tax Man: O Cavaleiro das Taxas", preco: 100000},
+    { id: 4, nome: "Taxarel", preco: 1000}
+];
+
+for(let i = 0; i <= 3; i++){
+    console.log(podutos[i])
+}
+
+podutos.push({id: 5, nome: "Taxa Humana", preco: 4})
+console.log(podutos.length)
+
+
+console.log(podutos.find(p => p.id === 3))
+
+const condicao = podutos.filter(p => p.preco >= 50);
+console.log(condicao)
